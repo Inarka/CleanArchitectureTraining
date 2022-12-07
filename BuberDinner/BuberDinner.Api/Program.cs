43e250c3da1957afcb702524e89bd7bfd699f369
inara <baby_inara@mail.ru>
2022-12-07 14:25:08 +0100

    lesson 1 finished

--- BuberDinner/BuberDinner.Api/Program.cs
+++ BuberDinner/BuberDinner.Api/Program.cs
@@ -1,5 +1,10 @@
+using BuberDinner.Application;
+using BuberDinner.Infrastrcuture;
+
 var builder = WebApplication.CreateBuilder(args);
 {
+	builder.Services.AddApplication();
+	builder.Services.AddInfrastructure();
 	builder.Services.AddControllers();
 }
 
